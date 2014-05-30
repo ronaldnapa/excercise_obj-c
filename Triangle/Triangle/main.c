790f8ae2a25f9e72dba7d635257ab47219a02e6b
ronaldnapa <r.napa@klab.com>
2014-05-30 12:15:38 +0800

    added Triangle

--- Triangle/Triangle/main.c
+++ Triangle/Triangle/main.c
@@ -8,11 +8,22 @@
 
 #include <stdio.h>
 
+float remainingAngle(float angleA, float angleB)
+{
+    float lastAngle = 180 - (angleA + angleB);
+    
+    return lastAngle;
+    
+}
+
 int main(int argc, const char * argv[])
 {
-
-    // insert code here...
-    printf("Hello, World!\n");
+    float angleA = 30.0;
+    float angleB = 60.0;
+    float angleC = remainingAngle(angleA, angleB);
+    
+    printf("The third angle is %.2f \n", angleC);
+    
     return 0;
 }
 
